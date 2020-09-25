aa8fb4a622c57ba53737d39839d79a5a1f434093
NickBonet <nbonet@kshadow.us>
2020-09-24 20:05:54 -0400

    Add base project files.

---
+++ src/MainGame.java
@@ -0,0 +1,16 @@
+/**
+ * MainGame - where all the magic is brought together!
+ * @author Nicholas Bonet
+ *
+ */
+
+public class MainGame {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+	}
+
+}
