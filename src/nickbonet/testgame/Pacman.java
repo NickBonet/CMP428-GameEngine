6f31c55605ac3052c00cdf78d79fd2f6e7a6c03b
NickBonet <nbonet@kshadow.us>
2020-10-22 10:53:05 -0400

    Move sprites into project directory.

--- src/nickbonet/testgame/Pacman.java
+++ src/nickbonet/testgame/Pacman.java
@@ -13,33 +13,33 @@ public class Pacman extends Sprite {
 	}
 	
 	protected void initAnimations() {
-		File restFrame = new File("C:\\Users\\nbonet\\Desktop\\pacman sprites\\pac_closed.png");
+		File restFrame = new File("assets/sprites/pacman/pac_closed.png");
 		Animation leftAnim = new Animation(65);
 		leftAnim.addFrame(restFrame);
-		leftAnim.addFrame(new File("C:\\Users\\nbonet\\Desktop\\pacman sprites\\pac_left_1.png"));
-		leftAnim.addFrame(new File("C:\\Users\\nbonet\\Desktop\\pacman sprites\\pac_left_2.png"));
-		leftAnim.addFrame(new File("C:\\Users\\nbonet\\Desktop\\pacman sprites\\pac_left_1.png"));
+		leftAnim.addFrame(new File("assets/sprites/pacman/pac_left_1.png"));
+		leftAnim.addFrame(new File("assets/sprites/pacman/pac_left_2.png"));
+		leftAnim.addFrame(new File("assets/sprites/pacman/pac_left_1.png"));
 		animDict.put("left", leftAnim);
 		
 		Animation upAnim = new Animation(65);
 		upAnim.addFrame(restFrame);
-		upAnim.addFrame(new File("C:\\Users\\nbonet\\Desktop\\pacman sprites\\pac_up_1.png"));
-		upAnim.addFrame(new File("C:\\Users\\nbonet\\Desktop\\pacman sprites\\pac_up_2.png"));
-		upAnim.addFrame(new File("C:\\Users\\nbonet\\Desktop\\pacman sprites\\pac_up_1.png"));
+		upAnim.addFrame(new File("assets/sprites/pacman/pac_up_1.png"));
+		upAnim.addFrame(new File("assets/sprites/pacman/pac_up_2.png"));
+		upAnim.addFrame(new File("assets/sprites/pacman/pac_up_1.png"));
 		animDict.put("up", upAnim);
 		
 		Animation downAnim = new Animation(65);
 		downAnim.addFrame(restFrame);
-		downAnim.addFrame(new File("C:\\Users\\nbonet\\Desktop\\pacman sprites\\pac_down_1.png"));
-		downAnim.addFrame(new File("C:\\Users\\nbonet\\Desktop\\pacman sprites\\pac_down_2.png"));
-		downAnim.addFrame(new File("C:\\Users\\nbonet\\Desktop\\pacman sprites\\pac_down_1.png"));
+		downAnim.addFrame(new File("assets/sprites/pacman/pac_down_1.png"));
+		downAnim.addFrame(new File("assets/sprites/pacman/pac_down_2.png"));
+		downAnim.addFrame(new File("assets/sprites/pacman/pac_down_1.png"));
 		animDict.put("down", downAnim);
 		
 		Animation rightAnim = new Animation(65);
 		rightAnim.addFrame(restFrame);
-		rightAnim.addFrame(new File("C:\\Users\\nbonet\\Desktop\\pacman sprites\\pac_right_1.png"));
-		rightAnim.addFrame(new File("C:\\Users\\nbonet\\Desktop\\pacman sprites\\pac_right_2.png"));
-		rightAnim.addFrame(new File("C:\\Users\\nbonet\\Desktop\\pacman sprites\\pac_right_1.png"));
+		rightAnim.addFrame(new File("assets/sprites/pacman/pac_right_1.png"));
+		rightAnim.addFrame(new File("assets/sprites/pacman/pac_right_2.png"));
+		rightAnim.addFrame(new File("assets/sprites/pacman/pac_right_1.png"));
 		animDict.put("right", rightAnim);
 	}
 }
