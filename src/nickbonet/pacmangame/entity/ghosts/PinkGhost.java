d696d4de68c8cdd21ba9df7734192d164e89959d
NickBonet <nbonet@kshadow.us>
2020-11-30 10:19:57 -0500

    Add comment noting behavior for Pinky.

--- src/nickbonet/pacmangame/entity/ghosts/PinkGhost.java
+++ src/nickbonet/pacmangame/entity/ghosts/PinkGhost.java
@@ -9,6 +9,9 @@ public class PinkGhost extends Ghost {
         super(x, y, "pink", delay, scatterTargetX, scatterTargetY);
     }
 
+    // Pinky's chase target tile is always 4 tiles in front of Pac-Man. In the original game, there was a bug
+    // where while Pac-Man was facing up, Pinky's target tile would be offset by 4 up and 4 away from Pac-Man's current tile.
+    // Not replicating that bug in this game.
     public void updateChaseTarget(Pacman player, TileMap map) {
         if (map.getNearbyTile(player.getBounds().getX(), player.getBounds().getY(), player.getSpriteDirection(), 4) != null) {
             Tile offsetTile = map.getNearbyTile(player.getBounds().getX(), player.getBounds().getY(),
